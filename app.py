c99b89132a68af4a2560cede7a8fe2226caec2f9
JiaWeiBu <93977802+JiaWeiBu@users.noreply.github.com>
2025-01-07 11:57:03 +0800

    Dependecy Rectify
    We can provide external registration for the messaging template
    
    Cogs Like tampleate created

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from os import getenv
+from enum import Enum, auto, unique
 from dotenv import load_dotenv
 from random import randint
 from discord import Client, Message, Intents
@@ -12,6 +13,75 @@ INTENTS.message_content = True
 CLIENT : Client = Client(intents=INTENTS)
 MESSAGE_UNIT = MessageUnit(keyword="~")
 
+@unique
+class ChannelEnum(Enum):
+    """
+    Enum class for the channel IDs
+
+    Attributes:
+    - log_: Log channel ID
+    - predict_: Prediction channel ID, user sends images and message to be answered
+    - debug_: Debug channel ID for train or debug purposes
+    """
+    log_ = auto()
+    predict_ = auto()
+    debug_ = auto()
+
+def SystemInit() -> None:
+    """
+    Function to initialize the system
+    """
+    RegisterChannel()
+
+# Temporary Placeholder
+async def ResLog(message : Message, message_object : MessageObject) -> None:
+        """
+        This is used as a logging for the system
+
+        This is a private channel for the system
+
+        TODO REMOVE THIS FUNCTION
+        """
+        message_object.SetMessage("Log : " + message.content)
+
+async def ResPredict(message : Message, message_object : MessageObject) -> None:
+    """
+    This is used for the prediction of the system
+    It will be samme like chatgpt
+
+    TODO REMOVE THIS FUNCTION
+    """
+    await message.create_thread(name="Prediction Thread", auto_archive_duration=1440)
+    if message.thread is None:
+        message_object.CreateEmbed(title="Thread", description="Thread is empty")
+    else:
+        for a in range(5):
+            await message.thread.send(f"Prediction {a}")
+        message_object.SetMessage("Predict : " + message.content)
+
+async def ResDebug(message : Message, message_object : MessageObject) -> None:
+    """
+    This is used for the debug of the system
+
+    This channel can accesss to hidden commands for the system.
+
+    This is a private channel for the system
+
+    TODO REMOVE THIS FUNCTION
+    """
+    message_object.SetFile("./datasets/re_plant/train/60/00001.jpg", filename="test.jpg", description="Test Description")
+    message_object.SetMessage("Debug : " + message.content)
+    message_object.CreateEmbed(title="Test Image", description="Test Image Description")
+    message_object.EmbedSetImage(url="attachment://test.jpg")
+
+def RegisterChannel() -> None:
+    """
+    Function to register the channels
+    """
+    MESSAGE_UNIT.RegisterChannelObject(channel=ChannelEnum.log_, channel_object=MESSAGE_UNIT.ChannelObject(ids=0, webhook_env="CHANNEL_WEBHOOK_LOG", webhook_url="", func=ResLog, password=0))
+    MESSAGE_UNIT.RegisterChannelObject(channel=ChannelEnum.predict_, channel_object=MESSAGE_UNIT.ChannelObject(ids=0, webhook_env="CHANNEL_WEBHOOK_PREDICT", webhook_url="", func=ResPredict, password=0))
+    MESSAGE_UNIT.RegisterChannelObject(channel=ChannelEnum.debug_, channel_object=MESSAGE_UNIT.ChannelObject(ids=0, webhook_env="CHANNEL_WEBHOOK_DEBUG", webhook_url="", func=ResDebug, password=0))
+
 
 async def SendMessage(message: Message) -> None:
     response : MessageObject = await MESSAGE_UNIT.GetResponse(message)
@@ -24,7 +94,7 @@ async def SendMessage(message: Message) -> None:
 async def on_ready() -> None:
     print(f'{CLIENT.user} has connected to Discord!')
     print(f"Setting up the channels")
-    for channel in MESSAGE_UNIT.ChannelEnum:
+    for channel in ChannelEnum:
         MESSAGE_UNIT.SetChannelWebhookUrl(channel=channel, webhook_url=str(getenv(MESSAGE_UNIT.channel_object_dict_[channel].webhook_env_)))
         MESSAGE_UNIT.SetChannelPass(channel=channel, pass_=randint(100000, 999999))
         await WebhookSend(webhook_url=MESSAGE_UNIT.channel_object_dict_[channel].webhook_url_, content=f"{MESSAGE_UNIT.keyword_}{INIT_PHRASE} {MESSAGE_UNIT.channel_object_dict_[channel].pass_}")
@@ -42,8 +112,8 @@ async def on_message(message: Message) -> None:
     await SendMessage(message)
 
 def run() -> None:
+    SystemInit()
     CLIENT.run(token=TOKEN)
 
 if __name__ == "__main__":
     run()
-
--- classes/message_lib.py
+++ classes/message_lib.py
@@ -33,7 +33,8 @@ class MessageUnit:
     4. Run the functions for the channels
 
     What to do:
-    1. Register the channels and their method
+    1. Create the channels Enum
+    2. Register the channels and their method
 
     Attributes:
     - ChannelEnum (Enum): Enum class for the channel IDs
@@ -45,7 +46,6 @@ class MessageUnit:
     - keyword_ (str): The keyword to check for the message.
 
     Classes:
-    - ChannelEnum: Enum class for the channel IDs
     - ChannelObject: Class for the channel object
 
     Methods:
@@ -67,20 +67,6 @@ class MessageUnit:
     >>> response = message_unit.GetResponse("message")
     >>> print(response)
     """
-    @unique
-    class ChannelEnum(Enum):
-        """
-        Enum class for the channel IDs
-
-        Attributes:
-        - log_: Log channel ID
-        - predict_: Prediction channel ID, user sends images and message to be answered
-        - debug_: Debug channel ID for train or debug purposes
-        """
-        log_ = auto()
-        predict_ = auto()
-        debug_ = auto()
-
     class ChannelObject:
         """
         Class for the channel object
@@ -127,23 +113,34 @@ class MessageUnit:
         - keyword (str): The keyword to check for the message.
 
         Attributes:
-        - channel_id_dict_inv_ (dict[int, ChannelEnum]): The dictionary for the channel ID.
-        - channel_object_dict_ (dict[ChannelEnum, ChannelObject]): The dictionary for the channel object.
+        - channel_id_dict_inv_ (dict[int, Enum]): Unordered Map for channel id and channel Enum
+        - channel_object_dict_ (dict[Enum, ChannelObject]): The dictionary for the channel object.
         - init_ (bool): The flag to check if the system is initialized.
         - num_channels_init_ (int): The number of channels to initialize.
         - keyword_ (str): The keyword to check for the message.
         """
-        self.channel_id_dict_inv_ : dict[int, self.ChannelEnum] = {}
-        self.channel_object_dict_ : dict[self.ChannelEnum, self.ChannelObject] = {
-            self.ChannelEnum.log_ : self.ChannelObject(ids=0, webhook_env="CHANNEL_WEBHOOK_LOG", webhook_url="", func=self.ResLog, password=0),
-            self.ChannelEnum.predict_ : self.ChannelObject(ids=0, webhook_env="CHANNEL_WEBHOOK_PREDICT", webhook_url="", func=self.ResPredict, password=0),
-            self.ChannelEnum.debug_ : self.ChannelObject(ids=0, webhook_env="CHANNEL_WEBHOOK_DEBUG", webhook_url="", func=self.ResDebug, password=0),
-        }
+        self.channel_id_dict_inv_ : dict[int, Enum] = {}
+        self.channel_object_dict_ : dict[Enum, self.ChannelObject] = {}
         self.init_ : bool = False
-        self.num_channels_init_ : int = len(self.channel_object_dict_)
+        self.num_channels_init_ : int = 0
         self.keyword_ : str = keyword
 
-    def SetChannelID(self, *, channel : ChannelEnum, channel_id : int) -> None:
+    def RegisterChannelObject(self, *, channel : Enum, channel_object : ChannelObject) -> None:
+        """
+        Adds the channel object to the channel object dictionary.
+
+        Args:
+        - channel (ChannelEnum): The channel to add the object for.
+        - channel_object (ChannelObject): The channel object to add.
+
+        Example:
+        >>> message_unit = MessageUnit()
+        >>> message_unit.AddChannelObject(channel=MessageUnit.ChannelEnum.log_, channel_object=MessageUnit.ChannelObject(ids=123456789, webhook_env="CHANNEL_WEBHOOK_LOG", webhook_url="", func=ResLog, password=123456789))
+        """
+        self.channel_object_dict_[channel] = channel_object
+        self.num_channels_init_ += 1
+
+    def SetChannelID(self, *, channel : Enum, channel_id : int) -> None:
         """
         Sets the channel ID for a given channel.
 
@@ -157,7 +154,7 @@ class MessageUnit:
         """
         self.channel_object_dict_[channel].id_ = channel_id
 
-    def SetChannelPass(self, *, channel : ChannelEnum, pass_ : int) -> None:
+    def SetChannelPass(self, *, channel : Enum, pass_ : int) -> None:
         """
         Sets the pass for the channel.
 
@@ -171,7 +168,7 @@ class MessageUnit:
         """
         self.channel_object_dict_[channel].pass_ = pass_
 
-    def SetChannelWebhookUrl(self, *, channel : ChannelEnum, webhook_url : str) -> None:
+    def SetChannelWebhookUrl(self, *, channel : Enum, webhook_url : str) -> None:
         """
         Sets the webhook URL for the channel.
 
@@ -284,43 +281,3 @@ class MessageUnit:
                     break
         else:
             message_object.SetMessage(f"System not initialized yet")
-
-    async def ResLog(self, message : Message, message_object : MessageObject) -> None:
-        """
-        This is used as a logging for the system
-
-        This is a private channel for the system
-
-        TODO REMOVE THIS FUNCTION
-        """
-        message_object.SetMessage("Log : " + message.content)
-
-    async def ResPredict(self, message : Message, message_object : MessageObject) -> None:
-        """
-        This is used for the prediction of the system
-        It will be samme like chatgpt
-
-        TODO REMOVE THIS FUNCTION
-        """
-        await message.create_thread(name="Prediction Thread", auto_archive_duration=1440)
-        if message.thread is None:
-            message_object.CreateEmbed(title="Thread", description="Thread is empty")
-        else:
-            for a in range(5):
-                await message.thread.send(f"Prediction {a}")
-            message_object.SetMessage("Predict : " + message.content)
-
-    async def ResDebug(self, message : Message, message_object : MessageObject) -> None:
-        """
-        This is used for the debug of the system
-
-        This channel can accesss to hidden commands for the system.
-
-        This is a private channel for the system
-
-        TODO REMOVE THIS FUNCTION
-        """
-        message_object.SetFile("./datasets/re_plant/train/60/00001.jpg", filename="test.jpg", description="Test Description")
-        message_object.SetMessage("Debug : " + message.content)
-        message_object.CreateEmbed(title="Test Image", description="Test Image Description")
-        message_object.EmbedSetImage(url="attachment://test.jpg")
